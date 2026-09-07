f30f05fc02a19ca669b17394260aa4dd95101dda
Nguyen Khac Trung Kien <trungkiennguyenkhac007@gmail.com>
2026-09-07 09:59:31 +0800

    Add VK M3 Lite protocol codecs and hardware evidence

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,4 @@ export * as corsair from "./corsair/index.js";
 export * as gwolves from "./gwolves/index.js";
 export * as glorious from "./glorious/index.js";
 export * as ksnake from "./ksnake/index.js";
+export * as valkyrie from "./valkyrie/index.js";
